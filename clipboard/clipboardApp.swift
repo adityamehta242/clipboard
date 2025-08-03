58081463e6031538e317a30692f80f935c67b13f
adityamehta242 <adityamehta242@gmail.com>
2025-08-03 14:26:16 +0530

    Initial Commit

---
+++ clipboard/clipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  clipboardApp.swift
+//  clipboard
+//
+//  Created by aditya mehta on 03/08/25.
+//
+
+import SwiftUI
+
+@main
+struct clipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
